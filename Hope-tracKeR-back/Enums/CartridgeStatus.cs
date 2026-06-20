f6a7a904c163c5632140b059241d26b9c91a19e7
Mcore-01 <dassd2123@gmail.com>
2026-06-20 08:55:36 +0500

    Добавил картридж

---
+++ Hope-tracKeR-back/Enums/CartridgeStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hope_tracKeR_back.Enums;
+
+public enum CartridgeStatus
+{
+    [Display(Name = "На складе")]
+    InStock,
+    [Display(Name = "Установлен")]
+    Installed,
+    [Display(Name = "Пустой")]
+    Empty,
+    [Display(Name = "Отправлен на заправку")]
+    Refilling,
+    [Display(Name = "Списан")]
+    WriteOff
+}
---
+++ Hope-tracKeR-back/Models/Entities/Cartridge.cs
@@ -0,0 +1,9 @@
+﻿using Hope_tracKeR_back.Enums;
+
+namespace Hope_tracKeR_back.Models.Entities;
+
+public class Cartridge : Item
+{
+    public CartridgeStatus Status { get; set; }
+    public string PrinterModel { get; set; } = null!;
+}
